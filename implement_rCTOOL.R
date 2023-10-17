0d578c90748d9e955b6ce61d16fb031fe4d0f795
Franca Giannini-Kurina <au710823@uni.au.dk>
2023-10-17 18:19:01 +0200

    dummy me

--- implement_rCTOOL.R
+++ implement_rCTOOL.R
@@ -1,10 +1,12 @@
 library(devtools)
-devtools::install_github("francagiannini/rCTOOL")
+# devtools::install_github("francagiannini/rCTOOL")
+# 
+# devtools::install_github("git@github.com:francagiannini/rCTOOL.git",
+#                      auth_token = "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")
 
-devtools::install_github("git@github.com:francagiannini/rCTOOL.git",
-                     auth_token = "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")
-
+devtools::install("C:/Users/au710823/OneDrive - Aarhus universitet/rCTOOL")
 
 library(rCTOOL)
-hello()
+
+hello(who="dummy Franca")
 
