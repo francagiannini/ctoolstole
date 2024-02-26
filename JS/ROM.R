6afdfb0ab7f4f5fd1011637178855c16de720976
Franca Giannini-Kurina <au710823@uni.au.dk>
2024-02-26 14:49:34 +0100

    with comments to add FOM transport in depth  as well

--- JS/ROM.R
+++ JS/ROM.R
@@ -17,10 +17,11 @@ ROM_top_calculations = function(ROM_top_t,
                                 s_config) {
   
   ROM_decomposition = soil_pool_decomposition(soil_pool=ROM_top_t, k=s_config[['k_rom']], soil_depth=25, month=month, t_avg=t_avg, t_range=t_range, s_config)
-  substrate_ROM_decomp_top = ROM_top_t - (ROM_top_t + ROM_decomposition) 
+  
+  substrate_ROM_decomp_top = ROM_top_t - (ROM_top_t + ROM_decomposition) # ROM_top_t - ROM_decomposition
   em_CO2_ROM_top = substrate_ROM_decomp_top * s_config[['f_co2']]
-  ROM_transport = (ROM_top_t+ROM_decomposition)  * s_config[['ftr']]
-  ROM_top = ROM_top_t - em_CO2_ROM_top - ROM_transport
+  ROM_transport = (ROM_top_t+ROM_decomposition)  * s_config[['ftr']] #substrate_ROM_decomp_top * s_config[['ftr']] 
+  ROM_top = ROM_top_t - em_CO2_ROM_top - ROM_transport 
   
   return(list(
     ROM_top = ROM_top,
@@ -43,15 +44,15 @@ ROM_top_calculations = function(ROM_top_t,
 #' @export
 #'
 #' @examples
-ROM_sub_calculations = function(ROM_sub_t,
+ROM_sub_calculations = function(ROM_sub_t, # here what I do not get is how we add to the previous ROM_sub what it comes from the transport of ROM_top
                                 month,
                                 t_avg = t_avg,
                                 t_range = t_range,
                                 s_config) {
   
   ROM_decomposition = soil_pool_decomposition(soil_pool=ROM_sub_t, k=s_config[['k_rom']], soil_depth=100, month=month, t_avg=t_avg, t_range=t_range, s_config = s_config)
-  substrate_ROM_decomp_sub = ROM_sub_t - (ROM_sub_t + ROM_decomposition) 
-  em_CO2_ROM_sub = substrate_ROM_decomp_sub * s_config[['f_co2']]
+  substrate_ROM_decomp_sub = ROM_sub_t - (ROM_sub_t + ROM_decomposition) # ROM_sub_t - ROM_decomposition
+  em_CO2_ROM_sub = substrate_ROM_decomp_sub * s_config[['f_co2']] 
   ROM_sub = ROM_sub_t - em_CO2_ROM_sub
   
   return(list(
